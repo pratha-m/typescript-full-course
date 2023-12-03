0953e4e1e8c96d9db00a25c022b35c3bd5e78adf
pratha-m <prathamchhabra85@gmail.com>
2023-12-03 23:26:04 +0530

    starting createing react todlist using ts

---
+++ react with typescript/react-todo-project1/src/App.tsx
@@ -0,0 +1,16 @@
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Todo App</Typography>
+        </Toolbar>
+      </AppBar>  
+      hdhdh
+    </Container>
+  )
+}
+
+export default App;
